a0b14245f8bbf249267b3c7213cca92a73517f8a
Caleb Simmons <calebsimmons39@gmail.com>
2025-05-08 09:37:35 +1200

    SubModule2 completed

---
+++ Module4/SubModule2/DataConvertChallenge1.cs
@@ -0,0 +1,16 @@
+//string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+// decimal running_total = 0;
+// string message = "";
+
+// foreach (string value in values){
+//     decimal result = 0;
+//     if(decimal.TryParse(value, out result)){
+//         running_total += result;
+//     }
+//     else{
+//         message += value;
+//     }
+// }
+// Console.WriteLine($"Message: {message}");
+// Console.WriteLine($"Total: {running_total}");
